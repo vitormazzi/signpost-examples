c0d8f7fdbbea68b0041b9d4b2d7d7601f35cdb9b
Vitor M. A. da Cruz <vitormazzi@gmail.com>
2011-10-25 12:51:55 -0200

    mv {MyfcID,PassaporteWeb}

--- OAuthPassaporteWebExample/src/PassaporteWeb.py
+++ OAuthPassaporteWebExample/src/PassaporteWeb.py
@@ -9,26 +9,26 @@ from  oauth.signpost import OAuthProvider;
 from  oauth.signpost.basic import DefaultOAuthConsumer;
 from  oauth.signpost.basic import DefaultOAuthProvider;
 
-CONSUMER_KEY = "uam9fsma9a87w7jm3v2r9n47";
-CONSUMER_SECRET = "Uze4zNHbJM";
+CONSUMER_KEY = "8ab29iwKFI";
+CONSUMER_SECRET = "VnWYenOqYsHtcFowrdJlwdJNALq5Go9v";
 
-CALLBACK_URL = "http://localhost/sso/callback";
-MYFCID_REQUEST_TOKEN_URL = "http://sandbox.id.myfreecomm.com.br/sso/initiate";
-MYFCID_ACCESS_TOKEN_URL = "http://sandbox.id.myfreecomm.com.br/sso/token";
-MYFCID_AUTHORIZE_URL = "http://sandbox.id.myfreecomm.com.br/sso/authorize";
-MYFCID_USER_DATA = "http://sandbox.id.myfreecomm.com.br/sso/fetchuserdata";
+CALLBACK_URL = OAuth.OUT_OF_BAND;
+REQUEST_TOKEN_URL = "http://sandbox.app.passaporteweb.com.br/sso/initiate";
+ACCESS_TOKEN_URL = "http://sandbox.app.passaporteweb.com.br/sso/token";
+AUTHORIZE_URL = "http://sandbox.app.passaporteweb.com.br/sso/authorize";
+USER_DATA = "http://sandbox.app.passaporteweb.com.br/sso/fetchuserdata";
 
-class MyfcIDMain():
+class PassaporteWebMain():
 
     def call(self):
 
         consumer = DefaultOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
 
         provider = DefaultOAuthProvider(
-            MYFCID_REQUEST_TOKEN_URL, MYFCID_ACCESS_TOKEN_URL, MYFCID_AUTHORIZE_URL
+            REQUEST_TOKEN_URL, ACCESS_TOKEN_URL, AUTHORIZE_URL
         );
 
-        print "Fetching request token from MyfcID..."
+        print "Fetching request token from PassaporteWeb..."
 
         authUrl = provider.retrieveRequestToken(consumer, CALLBACK_URL);
         authUrl = OAuth.addQueryParameters(authUrl, OAuth.OAUTH_CONSUMER_KEY, CONSUMER_KEY);
@@ -37,17 +37,18 @@ class MyfcIDMain():
         print "Token secret: ", consumer.getTokenSecret()
 
         print "Now visit:\n", authUrl, "\n... and grant this app authorization"
+        print "After granting authorization, you should be redirected to the callback url"
 
         pin = raw_input("Enter the PIN code and hit ENTER when you're done:")
 
-        print "Fetching access token from MyfcID..."
+        print "Fetching access token from PassaporteWeb..."
 
         provider.retrieveAccessToken(consumer, pin);
 
         print "Access token: ", consumer.getToken()
         print "Token secret: ", consumer.getTokenSecret()
 
-        url = URL(MYFCID_USER_DATA);
+        url = URL(USER_DATA);
         request = url.openConnection();
 
         consumer.sign(request);
@@ -60,5 +61,5 @@ class MyfcIDMain():
 
 if __name__ == '__main__':
 
-    myfc_id = MyfcIDMain()
+    myfc_id = PassaporteWebMain()
     myfc_id.call()
--- OAuthPassaporteWebExample/src/PassaporteWebMain.java
+++ OAuthPassaporteWebExample/src/PassaporteWebMain.java
@@ -9,25 +9,25 @@ import oauth.signpost.OAuthProvider;
 import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.basic.DefaultOAuthProvider;
 
-public class MyfcIDMain {
+public class PassaporteWebMain {
 
-    public static final String CONSUMER_KEY = "uam9fsma9a87w7jm3v2r9n47";
-    public static final String CONSUMER_SECRET = "Uze4zNHbJM";
+    public static final String CONSUMER_KEY = "8ab29iwKFI";
+    public static final String CONSUMER_SECRET = "VnWYenOqYsHtcFowrdJlwdJNALq5Go9v";
 
     public static final String CALLBACK_URL = OAuth.OUT_OF_BAND;
-    public static final String MYFCID_REQUEST_TOKEN_URL = "http://sandbox.id.myfreecomm.com.br/sso/initiate";
-    public static final String MYFCID_ACCESS_TOKEN_URL = "http://sandbox.id.myfreecomm.com.br/sso/token";
-    public static final String MYFCID_AUTHORIZE_URL = "http://sandbox.id.myfreecomm.com.br/sso/authorize";
-    public static final String MYFCID_USER_DATA = "http://sandbox.id.myfreecomm.com.br/sso/fetchuserdata";
+    public static final String REQUEST_TOKEN_URL = "http://sandbox.app.passaporteweb.com.br/sso/initiate";
+    public static final String ACCESS_TOKEN_URL = "http://sandbox.app.passaporteweb.com.br/sso/token";
+    public static final String AUTHORIZE_URL = "http://sandbox.app.passaporteweb.com.br/sso/authorize";
+    public static final String USER_DATA = "http://sandbox.app.passaporteweb.com.br/sso/fetchuserdata";
 
     public static void main(String[] args) throws Exception {
 
         OAuthConsumer consumer = new DefaultOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
 
-        OAuthProvider provider = new DefaultOAuthProvider(MYFCID_REQUEST_TOKEN_URL,
-            MYFCID_ACCESS_TOKEN_URL, MYFCID_AUTHORIZE_URL);
+        OAuthProvider provider = new DefaultOAuthProvider(REQUEST_TOKEN_URL,
+            ACCESS_TOKEN_URL, AUTHORIZE_URL);
 
-        System.out.println("Fetching request token from MyfcID...");
+        System.out.println("Fetching request token from PassaporteWeb...");
 
         String authUrl = provider.retrieveRequestToken(consumer, CALLBACK_URL);
         authUrl = OAuth.addQueryParameters(authUrl, OAuth.OAUTH_CONSUMER_KEY, CONSUMER_KEY);
@@ -41,14 +41,14 @@ public class MyfcIDMain {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String pin = br.readLine();
 
-        System.out.println("Fetching access token from MyfcID...");
+        System.out.println("Fetching access token from PassaporteWeb...");
 
         provider.retrieveAccessToken(consumer, pin);
 
         System.out.println("Access token: " + consumer.getToken());
         System.out.println("Token secret: " + consumer.getTokenSecret());
 
-        URL url = new URL(MYFCID_USER_DATA);
+        URL url = new URL(USER_DATA);
         HttpURLConnection request = (HttpURLConnection) url.openConnection();
 
         consumer.sign(request);
